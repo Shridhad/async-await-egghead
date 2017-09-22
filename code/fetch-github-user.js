098ff92f784baf80e53cf57ec7e951b568d9a0b1
Shridhar Deshmukh <shridhar.deshmukh3@gmail.com>
2017-09-22 21:43:42 +0530

    #1 - Fetch github user

---
+++ code/fetch-github-user.js
@@ -0,0 +1,12 @@
+const fetch = require("node-fetch");
+
+async function showGithubUser(handle) {
+  const url = `https://api.github.com/users/${handle}`;
+
+  const response = await fetch(url);
+  const user = await response.json();
+
+  console.log(user.name, user.location);
+}
+
+showGithubUser("shridhad");
